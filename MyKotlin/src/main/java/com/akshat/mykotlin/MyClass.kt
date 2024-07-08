9de5da3baa8637979f7323f2debeb8efdc888607
Akshat <akshat@Akshats-MacBook-Pro.local>
2024-07-08 11:19:32 -0400

    Difference between var and val in Kotlin

--- MyKotlin/src/main/java/com/akshat/mykotlin/MyClass.kt
+++ MyKotlin/src/main/java/com/akshat/mykotlin/MyClass.kt
@@ -1,6 +1,12 @@
 package com.akshat.mykotlin
 
 fun main(){
-    val name = "Akshat"
+
+
+    /**
+     * Difference between var and val in Kotlin
+     * */
+    // var name = "Kotlin" //mutable
+    val name = "Akshat"    // immutable
     print("Hello $name")
 }
