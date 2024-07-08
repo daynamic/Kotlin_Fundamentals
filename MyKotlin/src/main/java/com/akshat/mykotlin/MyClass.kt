8ca0225a6720c74494306f7e03643454184b2efc
Akshat <akshat@Akshats-MacBook-Pro.local>
2024-07-08 11:28:50 -0400

    Another way to initialize variables

--- MyKotlin/src/main/java/com/akshat/mykotlin/MyClass.kt
+++ MyKotlin/src/main/java/com/akshat/mykotlin/MyClass.kt
@@ -5,8 +5,21 @@ fun main(){
 
     /**
      * Difference between var and val in Kotlin
+     *  var name = "Kotlin" //mutable
+     *  val name = "Akshat" // immutable
+     * Best practice - In essence use val over var
      * */
-    // var name = "Kotlin" //mutable
-    val name = "Akshat"    // immutable
-    print("Hello $name")
+
+
+    /**
+     * Another way to initialize variables
+     * val name: String
+     * name = "James"
+     * */
+
+
+
+
+    
+    print("Hello my name is $name I 'm $age old")
 }
